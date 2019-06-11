5f5081a12476ab4e5495056cb2ab96ce77487a12
pezeli2002 <37782390+pezeli2002@users.noreply.github.com>
2019-06-11 11:19:04 -0700

    Add files via upload

---
+++ consul.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export NODE_NAME=$1
+hostnamectl set-hostname $NODE_NAME
+echo “127.0.0.1 $NODE_NAME” >> /etc/hosts
+
+cp /vagrant/$NODE_NAME.d/consul.service /lib/systemd/system/
+cp /vagrant/consul.hcl /etc/consul.d
+cp /vagrant/$NODE_NAME.d/server.hcl /etc/consul.d
+systemctl enable consul
+systemctl start consul
